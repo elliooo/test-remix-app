c77cbb636987b74573d724110a406d49cc0ba1e4
Elliot Park <elliot.jun.park@gmail.com>
2023-04-17 13:50:33 -0700

    Converts existing components into functional components

--- app/root.tsx
+++ app/root.tsx
@@ -7,21 +7,21 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
-export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
-}
+const App = () => (
+  <html lang="en">
+    <head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <Meta />
+      <Links />
+    </head>
+    <body>
+      <Outlet />
+      <ScrollRestoration />
+      <Scripts />
+      <LiveReload />
+    </body>
+  </html>
+);
+
+export default App;
